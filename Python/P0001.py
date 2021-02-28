56206330e56b907d25be60719c5ee11c4e91f0e2
lin-ao <58115732+lin-ao@users.noreply.github.com>
2021-02-28 10:23:11 +0100

    added assertion condition

--- Python/P0001.py
+++ Python/P0001.py
@@ -2,8 +2,13 @@ def check_multiples(number: int) -> bool:
     return number % 3 == 0 or number % 5 == 0
 
 
+def sum_multiples(upper: int) -> int:
+    return sum(filter(check_multiples, range(upper)))
+
+
 def main() -> None:
-    answer = sum(filter(check_multiples, range(1000)))
+    assert sum_multiples(10) == 23
+    answer = sum_multiples(1000)
     print(f"Answer: {answer}")
 
 
