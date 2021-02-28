b513dc2a143132b15e12b09dd069b865037cca41
lin-ao <58115732+lin-ao@users.noreply.github.com>
2021-02-28 11:28:11 +0100

    initial commit

---
+++ Python/P0005.py
@@ -0,0 +1,11 @@
+from math import lcm
+
+
+def main() -> None:
+    assert lcm(*range(1, 11)) == 2520
+    answer = lcm(*range(1, 21))
+    print(f"Answer: {answer}")
+
+
+if __name__ == '__main__':
+    main()
