7899af35e024f0723d8b18c4597db35a87a66eb0
lin-ao <lin.ao@web.de>
2021-03-17 17:30:37 +0100

    initial commit

---
+++ Python/P0015.py
@@ -0,0 +1,16 @@
+from math import factorial
+
+
+#Permutations of multisets
+def calculate_paths(side: int) -> int:
+    return factorial(side * 2) // (factorial(side) * factorial(side))
+
+
+def main() -> None:
+    assert calculate_paths(2) == 6
+    answer = calculate_paths(20)
+    print(f"Answer: {answer}")
+
+
+if __name__ == "__main__":
+    main()
